1dba0b66dd142eafc19ddeeefa628b799b193c40
M-Facey <faceymaxal17@gmail.com>
2023-07-10 11:20:55 -0500

    feat: added routes for home pages

--- client/src/router/index.ts
+++ client/src/router/index.ts
@@ -1,8 +1,12 @@
 import { createRouter, createWebHistory } from "vue-router";
 
-// components
+// pages
 import Home from "@/pages/Home.vue";
 import NotFound from "@/pages/NotFound.vue";
+
+// components
+import GameSettings from "@/components/home/GameSettings.vue";
+import GameMode from "@/components/home/GameMode.vue";
 
 export default createRouter({
   history: createWebHistory(),
@@ -11,6 +15,23 @@ export default createRouter({
       path: "/",
       name: "home",
       component: Home,
+      children: [
+        {
+          path: "",
+          name: "no-mode",
+          component: GameMode,
+        },
+        {
+          path: "/play/friend",
+          name: "friend-mode",
+          component: GameSettings,
+        },
+        {
+          path: "/play/computer",
+          name: "computer-mode",
+          component: GameSettings,
+        },
+      ],
     },
     {
       path: "/:pathMatch(.*)*",
