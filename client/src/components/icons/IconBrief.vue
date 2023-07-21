11d3019a0bb58dcdf452bef4e183d52f023b9b65
M-Facey <faceymaxal17@gmail.com>
2023-07-21 14:17:12 -0500

    feat: added board icons

---
+++ client/src/components/icons/IconBrief.vue
@@ -0,0 +1,21 @@
+<template>
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    width="1em"
+    height="1em"
+    viewBox="0 0 24 24"
+  >
+    <g
+      fill="none"
+      stroke="currentColor"
+      strokeLinecap="round"
+      strokeLinejoin="round"
+      strokeWidth="2"
+    >
+      <path
+        d="M3 9a2 2 0 0 1 2-2h14a2 2 0 0 1 2 2v9a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2zm5-2V5a2 2 0 0 1 2-2h4a2 2 0 0 1 2 2v2m-4 5v.01"
+      ></path>
+      <path d="M3 13a20 20 0 0 0 18 0"></path>
+    </g>
+  </svg>
+</template>
---
+++ client/src/components/icons/IconShuffle.vue
@@ -0,0 +1,21 @@
+<template>
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    width="1em"
+    height="1em"
+    viewBox="0 0 24 24"
+  >
+    <g
+      fill="none"
+      stroke="currentColor"
+      strokeLinecap="round"
+      strokeLinejoin="round"
+      strokeWidth="2"
+    >
+      <path d="m18 4l3 3l-3 3m0 10l3-3l-3-3"></path>
+      <path
+        d="M3 7h3a5 5 0 0 1 5 5a5 5 0 0 0 5 5h5m0-10h-5a4.978 4.978 0 0 0-3 1m-4 8a4.984 4.984 0 0 1-3 1H3"
+      ></path>
+    </g>
+  </svg>
+</template>
